57757be5bbba85e05e9270a471ced156b1d1a631
Juan Pablo Solano <juanpablosolanosuarez@hotmail.com>
2019-09-25 04:58:16 -0700

    Update Twilio_API.rb

--- Twilio_API.rb
+++ Twilio_API.rb
@@ -1,7 +1,7 @@
 require 'twilio-ruby'
 
-account_sid = 'AC99014ee83d797175018ec9f5b70f9128'
-auth_token = 'd8725d8caa7f806b8e511e0c251b2de0'
+account_sid = 'AC99014ee83d797175018'
+auth_token = 'd8725d8caa7f806b8e511'
 client = Twilio::REST::Client.new(account_sid, auth_token)
 
 from = '+12029153365' # Your Twilio number
